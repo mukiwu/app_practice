05e224b04e27a6c151b6dba21e1f300122698d44
mukiwu <mukispace@gmail.com>
2016-04-07 19:26:09 +0800

    Initial Commit

---
+++ mukiFirstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mukiFirstApp
+//
+//  Created by muki on 2016/4/7.
+//  Copyright © 2016年 mukispace. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
